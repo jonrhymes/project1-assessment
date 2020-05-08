75d12ba81967b4060cab4ecd5984dfd152065d4f
jonrhymes <63318712+jonrhymes@users.noreply.github.com>
2020-05-08 13:22:29 -0400

    130pm commit

--- app.js
+++ app.js
@@ -4,47 +4,64 @@
 // ========>
 // Functions
 
-let num = 0;
+// let num = 0;
 // num++
 
-const add = () => {
-    return ++num;
-}
-
-// console.log(add());
-
-const minus = () => {
-    return --num;
-}
-
-// minus();
-// minus();
-// console.log(minus())
-// const minus = (num) => {
-//     num = num--;
+// const add = () => {
+//     return ++num;
 // }
-// minus();
-// console.log(num)
+
+// const add = () => {
+//     let newNum = parseInt($input.val())++;
+// }
+
+// const minus = () => {
+//     return --num;
+// }
 
 
 // ========>
 // Event Handlers
 
-const $count = $('.counter');
-const $input = $('.set-num');
+let $count = $('.counter');
+let $input = $('.set-num');
 const $add = $('.plus');
 const $subtract = $('.minus');
+let num = parseInt($count.html());
 
+console.log($count.html())
+console.log($input.val())
+console.log($count.html())
 
+// !! Problem: I am able to update the value of the counter but I cannot pass it to the next argument
+
+// console.log(parseInt($input.val()))
+// console.log(parseInt($count.html()))
+// console.log(newnum)
+
+$input.change(() => {
+    $count.html($input.val());
+})
+
+const add = () => {
+    return ++num;
+}
+
+const minus = () => {
+    return --num;
+}
+
+// console.log(parseInt($count.html()));
+// let num = parseInt($count.html());
 
 $add.on('click', () => {
     add();
+    $count.html(num);
     console.log(num)
 })
 
 $subtract.on('click', () => {
     minus();
+    $count.html(num);
     console.log(num)
 })
-
-console.log($count.text())
